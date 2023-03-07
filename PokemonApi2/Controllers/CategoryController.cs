79b9eef835256cd7b010e875011ef041ab7c136b
Martekode <BrianDesmedt9000@hotmail.com>
2023-03-07 05:01:43 +0100

    added category delete func

--- PokemonApi2/Controllers/CategoryController.cs
+++ PokemonApi2/Controllers/CategoryController.cs
@@ -127,5 +127,28 @@ namespace PokemonApi2.Controllers
             return NoContent();
         }
 
+        [HttpDelete("categoryId")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteCategory(int categoryId)
+        {
+            if(!_categoryRepository.CategoryExists(categoryId))
+                return NotFound();
+
+            var categoryToDelete = _categoryRepository.GetCategory(categoryId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_categoryRepository.DeleteCategory(categoryToDelete))
+            {
+                ModelState.AddModelError("", "Something went wrong when trying to delete the category.");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
     }
 }
--- PokemonApi2/Repository/CategoryRepository.cs
+++ PokemonApi2/Repository/CategoryRepository.cs
@@ -23,6 +23,12 @@ namespace PokemonApi2.Repository
             return Save();
         }
 
+        public bool DeleteCategory(Category category)
+        {
+            _context.Remove(category);
+            return Save();
+        }
+
         public ICollection<Category> GetCategories()
         {
             return _context.Categories.OrderBy(c => c.ID).ToList();

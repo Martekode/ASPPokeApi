48e16c1037869342e276782e9337f3e70b01d547
Martekode <BrianDesmedt9000@hotmail.com>
2023-03-07 05:13:13 +0100

    added delete country func

--- PokemonApi2/Interfaces/ICountryRepository.cs
+++ PokemonApi2/Interfaces/ICountryRepository.cs
@@ -11,6 +11,7 @@ namespace PokemonApi2.Interfaces
         bool CountryExists(int countryId);
         bool CreateCountry(Country country);
         bool UpdateCountry(Country country);
+        bool DeleteCountry(Country country);
         bool Save();
     }
 }
